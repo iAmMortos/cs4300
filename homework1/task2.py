a78cc87fc4ebab5df77348e211e4631bf5ef924b
iAmMortos <taylor0lopez@gmail.com>
2025-01-31 08:48:04 +0000

    adding start to task2

--- homework1/task2.py
+++ homework1/task2.py
@@ -1,14 +1,34 @@
 
-def main():
-  # integers
-  # floating point
-  # numbers
-  # strings
-  # boolean
-  #
-  # test each
-  pass
+from numbers import Number
 
 
-if __name__ == '__main__':
-  main()
+def add_integers(a:int, b:int) -> int:
+  return a + b
+
+
+def add_floats(a:float, b:float) -> float:
+  return a + b
+
+
+def add_numbers(a:Number, b:Number) -> Number:
+  return a + b
+
+
+def add_ducks(a, b):
+  return a + b
+
+
+def add_strings(a:str, b:str) -> str:
+  return a + b
+
+
+def add_booleans(a:bool, b:bool) -> bool:
+  return a + b
+
+
+def and_booleans(a:bool, b:bool) -> bool:
+  return a & b
+
+
+def xor_booleans(a:bool, b:bool) -> bool:
+  return a ^ b
--- homework1/tests/test_task2.py
+++ homework1/tests/test_task2.py
@@ -1,7 +1,16 @@
 
 import __init__
 import task2
+import pytest
 
 
-def test_task2():
-  pass
+def test_add_integers():
+  val = task2.add_integers(2, 5)
+  assert type(val) is int
+  assert val == 7
+  assert task2.add_integers(1.0, 2.5) == 3.5
+  
+  with pytest.raises(Exception) as exc_info:
+    raise Exception('some info')
+  assert exc_info.value.args[0] == 'some info'
+  assert str(exc_info.value) == 'some info'
