743ebc59f8137272d6b3cfdd28709c1a21270333
Mortos Der Soulstealer <iAmMortos@users.noreply.github.com>
2025-02-04 17:59:25 -0700

    sync

--- homework1/task4.py
+++ homework1/task4.py
@@ -5,16 +5,29 @@ it'll be treated as a percentage already and be directly applied.
 
 If the discount_percent_off is given as a value over 1, it'll be treated
 as a value that needs to be divided by 100 first.
+
+This function assumes a discound less than 1% will ever be required.
 """
 def calculate_discount(price, discount_percent_off):
   if 0 <= discount_percent_off <= 1:
-    pass
+    return price * (1 - discount_percent_off)
   elif 1 < discount_percent_off <= 100:
-    pass
+    return price * (1 - (discount_percent_off / 100))
   else:
-    pass
-  
+    raise Exception("Invalid discount value given. Must be in range [0, 100]")
+    
+
+def price_as_string(price):
+  return f"${price:.2f}"
 
 
 if __name__ == '__main__':
-  pass
+  print(price_as_string(calculate_discount(20.00, 20)))
+  print(price_as_string(calculate_discount(20.00, 0.2)))
+  print(price_as_string(calculate_discount(30, 33.33)))
+  print(price_as_string(calculate_discount(30, 1/3)))
+  print(price_as_string(calculate_discount(50, 33.33)))
+  print(price_as_string(calculate_discount(50, 1/3)))
+  print(price_as_string(calculate_discount(100, 0)))
+  print(price_as_string(calculate_discount(5, 100)))
+
--- homework1/tests/test_task4.py
+++ homework1/tests/test_task4.py
@@ -1,4 +1,14 @@
 import __init__
 import task4
 
-pass
+
+def test_calculate_discount():
+  assert task4.calculate_discount(20.00, 20) == 16
+  assert task4.calculate_discount(20.00, 0.2) == 16
+  assert task4.calculate_discount(30, 33.33) == 20
+  assert task4.calculate_discount(30, 1/3) == 20
+  assert task4.calculate_discount(50, 33.33) == 33.33
+  assert task4.calculate_discount(50, 1/3) == 33.33
+  assert task4.calculate_discount(100, 0) == 100
+  assert task4.calculate_discount(5, 100) == 0
+
